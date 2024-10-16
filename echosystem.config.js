c4e2ca0c26b71d94fcffaad6f6bb4295cf04cb2a
Nethmee5 <kumararatnenethmee@gmail.com>
2024-10-16 17:04:37 +0800

    echosystem.config.js file change

--- echosystem.config.js
+++ echosystem.config.js
@@ -1,17 +1,9 @@
 module.exports = {
-    apps: [
-      {
-        name: "online-test-platform",               
-        script: "src/App.js",          
-        instances: 1,                  
-        autorestart: true,          
-        max_memory_restart: "1G",      
-        env: {
-          NODE_ENV: "development",    
-        },
-        env_production: {
-          NODE_ENV: "production",     
-        },
-      },
-    ],
-  };
+  apps: [
+    {
+      name: "frontend",
+      script: "npm",
+      args: ['start'],
+  },      
+  ],
+};
